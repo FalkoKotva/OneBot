6828d875a065fe9cbf37eb5e750c9f24081c7f12
jamesparkin675 <james.parkin2004@gmail.com>
2022-09-09 20:33:56 +0100

    Music Cog
    WIP

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,6 @@ This file contains all the constants used in the bot.
 """
 
 
-GUILD_ID = 1017473530151059548
+GUILD_ID = 912068802005569557
 TICKET_SUBMITTED = 'Ticket submitted. This command is under going testing and should not be used by server members right now.'
 DATABASE = 'db.sqlite3'
