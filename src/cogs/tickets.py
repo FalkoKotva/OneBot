17c83d4b680dcba53aa6f9e0f6ee8bd58464df35
Corban-Lee <843224@student.derby-college.ac.uk>
2022-09-09 21:37:08 +0100

    fixed interaction with db

--- src/cogs/tickets.py
+++ src/cogs/tickets.py
@@ -2,12 +2,13 @@
 Cog for handling tickets.
 """
 
+
+import aiosqlite
 import discord
-from discord.ext import commands
 from discord import app_commands
 
 from cog import Cog
-from constants import GUILD_ID
+from constants import DATABASE, GUILD_ID, TICKET_SUBMITTED
 
 
 guild = discord.Object(GUILD_ID)
@@ -21,13 +22,41 @@ class Tickets(Cog):
     def __init__(self, bot):
         super().__init__(bot)
 
-    @app_commands.command(name='ticket')
+    @app_commands.command(name='report-ticket')
     @app_commands.guilds(guild)
-    async def ticket(self, interaction: discord.Interaction):
+    async def create_ticket(self, interaction: discord.Interaction, offender:discord.Member, *, reason: str):
         """
-        Create a ticket
+        Create a report ticket. Use this if another user has broken
+        the rules or is harassing you.
         """
-        await interaction.response.send_message('Slash commands are working!')
+        
+        await interaction.response.defer()
+
+        async with aiosqlite.connect(DATABASE) as db:
+            await db.execute(
+                """
+                INSERT INTO user_report_tickets (userId, offenderUserId, messageContent) 
+                VALUES (?, ?, ?)
+                """,
+                (interaction.user.id, offender.id, reason)
+            )
+            await db.commit()
+            
+            ticket_id = await db.execute_fetchall("SELECT * FROM user_report_tickets")
+            ticket_id = ticket_id[-1][0]
+
+        guild: discord.Guild = self.bot.main_guild
+        print('guild', guild)
+        category: discord.CategoryChannel = discord.Object(id=1017881107930304533)
+        print('category', category)
+        try:
+            channel = await guild.create_text_channel(name="ticket")
+        except Exception as e:
+            print(e)
+        print('channel', channel)
+
+        await interaction.followup.send(TICKET_SUBMITTED)
+        
         
     
 
