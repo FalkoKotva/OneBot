09ad18918c1948ae8bc6cb23ec36f5c7f39468e1
Corban-Lee <843224@student.derby-college.ac.uk>
2022-09-09 19:50:55 +0100

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,5 @@ This file contains all the constants used in the bot.
 
 
 GUILD_ID = 1017473530151059548
+TICKET_SUBMITTED = 'Ticket submitted. This command is under going testing and should not be used by server members right now.'
+DATABASE = 'db.sqlite3'
