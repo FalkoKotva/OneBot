932a838f395c957ce3913892dbd80930a5f4a221
jamesparkin675 <james.parkin2004@gmail.com>
2022-09-09 20:56:35 +0100

    Updated Guild
    wip

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,6 @@ This file contains all the constants used in the bot.
 """
 
 
-GUILD_ID = 912068802005569557
+GUILD_ID = 1004829607024918718
 TICKET_SUBMITTED = 'Ticket submitted. This command is under going testing and should not be used by server members right now.'
 DATABASE = 'db.sqlite3'
