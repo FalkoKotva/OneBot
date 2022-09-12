f3723dba7f7758dfd6dcbd3df916b162f3bb95ac
Corban-Lee <843224@student.derby-college.ac.uk>
2022-09-12 15:11:18 +0100

    added functional suggestion and report modals

--- src/ui.py
+++ src/ui.py
@@ -5,9 +5,14 @@ discord UI kit objects.
 import discord
 import discord.ui as ui
 from discord import Interaction
+from typing import Coroutine
 
 
 class ReportModal(ui.Modal, title='Report Ticket'):
+    """
+    Modal for users to report other users.
+    """
+    
     accused = ui.TextInput(
         label='Who are you reporting?',
         placeholder='exampleuser#1234'
@@ -17,8 +22,37 @@ class ReportModal(ui.Modal, title='Report Ticket'):
         style=discord.TextStyle.long
     )
     
-    async def on_submit(self, interaction: Interaction, /) -> None:
+    def __init__(self, func: Coroutine):
+        super().__init__()
+        self.func = lambda *args: func(*args)
+    
+    async def on_submit(self, interaction: Interaction):
         await interaction.response.send_message(
-            'Thanks for your report {0.user.mention}!'.format(interaction) /
-            'We will look into it as soon as possible.'
+            'Thanks for your report {0.user.mention}!'.format(interaction) +
+            '\nWe will look into it as soon as possible.',
+            ephemeral=True
         )
+        await self.func(self.accused.value, self.reason.value)
+
+
+class SuggestionModal(ui.Modal, title='Suggestion Ticket'):
+    """
+    Modal for users to submit suggestions for the server.
+    """
+    
+    suggestion = ui.TextInput(
+        label='What suggestion/feature request would you like to share?',
+        style=discord.TextStyle.long
+    )
+    
+    def __init__(self, func: Coroutine):
+        super().__init__()
+        self.func = lambda *args: func(*args)
+    
+    async def on_submit(self, interaction: Interaction):
+        await interaction.response.send_message(
+            'Thanks for your suggestion {0.user.mention}!'.format(interaction) +
+            '\nWe will look into it as soon as possible.',
+            ephemeral=True
+        )
+        await self.func(self.suggestion.value)
