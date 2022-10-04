f29ec91bf7d34be5b8b4d8c7923ba4f71d57ff3c
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-10-04 20:10:52 +0100

    added member_remove message

--- src/cogs/welcome.py
+++ src/cogs/welcome.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 from datetime import datetime
 
 from cog import Cog
-from constants import Channels
+from constants import GUILD_ID, Channels
 
 
 class Welcome(Cog):
@@ -25,19 +25,49 @@ class Welcome(Cog):
         """
 
         embed = await self.get_welcome_embed(member)
-        welcome_channel = self.bot.get_channel(Channels.WELCOME)
-        await welcome_channel.send(embed=embed)
+        channel = self.bot.get_channel(Channels.WELCOME)
+        await channel.send(embed=embed)
+        
+    @commands.Cog.listener()
+    async def on_member_remove(self, member:discord.Member):
+        """Sends a message when a member leaves the server
+
+        Args:
+            member (discord.Member): The member that left.
+        """
+
+        embed = await self.get_remove_embed(member)
+        channel = self.bot.get_channel(Channels.WELCOME)
+        await channel.send(embed=embed)
     
-    @app_commands.command(name='welcometest')
+    group = app_commands.Group(
+        name='wtest',
+        description='Test the join/leave events',
+        guild_ids=(GUILD_ID,),
+        default_permissions=discord.Permissions(moderate_members=True)
+    )
+    
+    @group.command(name='join')
     async def welcome_test(self, interaction:Interaction, member:discord.Member):
         """Test command for the welcome view and embed"""
 
-        # Get and send the view + embed
+        # Get and send the embed
         embed = await self.get_welcome_embed(member)
         await interaction.channel.send(embed=embed)
 
         # Acknowledge the interaction to prevent an error
         await interaction.response.send_message('done')
+    
+    @group.command(name='remove')
+    async def remove_test(self, inter:Interaction, member:discord.Member):
+        """Test command for the remove embed"""
+
+        # Get and send the embed
+        embed = await self.get_remove_embed(member)
+        await inter.channel.send(embed=embed)
+
+        # Acknowledge the interaction to prevent an error
+        await inter.response.send_message('done')
     
     async def get_welcome_embed(
         self, member:discord.Member, /
@@ -89,6 +119,29 @@ class Welcome(Cog):
         embed.set_thumbnail(url=member.avatar.url)
         embed.set_footer(text='DCG Server', icon_url=rules_channel.guild.icon.url)
 
+        return embed
+
+    async def get_remove_embed(self, member:discord.Member):
+        """_summary_
+
+        Args:
+            member (discord.Member): _description_
+        """
+        
+        # The embed base
+        embed = discord.Embed(
+            title='Goodbye, you won\'t be missed!',
+            description=f'{member.mention} has left the server.',
+            colour=discord.Colour.from_str('#00FEFE'),
+            timestamp=datetime.now()
+        )
+
+        icon_url = self.bot.get_guild(GUILD_ID).icon.url
+
+        # Thumbnail and footer for the embed
+        embed.set_thumbnail(url=member.avatar.url)
+        embed.set_footer(text='DCG Server', icon_url=icon_url)
+        
         return embed
 
 
