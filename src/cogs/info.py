54c8835a218c401c1eb1548f8c93f932b1e6e485
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-10-06 21:10:01 +0100

    added latency to server info

--- src/cogs/info.py
+++ src/cogs/info.py
@@ -19,14 +19,16 @@ class InfoCog(Cog, name='Info'):
         super().__init__(bot=bot)
         self.get_app_commands()[0].guilds = (bot.main_guild.id,)
 
-    @app_commands.command(name='info')
+    @app_commands.command(name='serverinfo')
     async def get_all_info(self, inter:Inter):
         """Get all info on the bot."""
 
+        # Get the info
         dpy_ver = discord.__version__
         py_ver = platform.python_version()
         uptime = str(self.bot.uptime)
         start_time = str(self.bot.start_time)
+        latency = round(self.bot.latency*1000, 2)
 
         embed = discord.Embed(
             title='Server Details',
@@ -36,6 +38,7 @@ class InfoCog(Cog, name='Info'):
             '---\n'
             f'OS: {platform.system()}\n'
             f'OS Ver: {platform.version()}\n'
+            f'Latency {latency}ms\n'
             '---\n'
             f'Uptime: {uptime}\n'
             f'Started: {start_time}\n'
