e819abd9ae064271ed86bbc5160662c2728aaa64
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-10-08 16:05:06 +0100

    Patch invalid type error

--- src/cogs/birthday.py
+++ src/cogs/birthday.py
@@ -119,7 +119,7 @@ class BirthdayCog(BaseCog, name='Birthdays'):
             )
 
         bday_list = [
-            f'{member.mention}: {bday}' for member, bday in data
+            f'{inter.guild.get_member(uid).mention}: {bday}' for uid, bday in data
         ]
 
         await inter.response.send_message(
