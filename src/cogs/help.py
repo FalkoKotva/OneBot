b9d34a921493f042614e38158ea1632224cd42bf
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-10-15 13:11:22 +0100

    Added multi page embed and help cmd

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -1,11 +1,17 @@
 """Help cog for discord members who need help."""
 
+import logging
+
 import discord
 from discord import app_commands
 from discord import Interaction as Inter
 
-from ui import HelpChannelsEmbed
+from utils import normalized_name
+from ui import EmbedPageManager, HelpChannelsEmbed
 from . import BaseCog
+
+
+log = logging.getLogger(__name__)
 
 
 class HelpCog(BaseCog):
@@ -23,10 +29,42 @@ class HelpCog(BaseCog):
     async def channels(self, inter:Inter):
         """Get a list of all channels and their topics"""
 
-        embed = HelpChannelsEmbed(inter.guild.channels)
-        await inter.response.send_message(embed=embed, ephemeral=True)
+        # Log the interaction including who used it
+        name = normalized_name(inter.user, with_id=False)
+        log.debug('%s requested help channels', name)
 
+        sorted_channels: list[list[discord.TextChannel]] = []
+        chars = 0  # The length of characters in the current page
 
+        for channel in inter.guild.text_channels:
+
+            log.debug('Checking channel: %s', channel.name)
+
+            # Add the character counts to determine if the page is full
+            chars += len(channel.name)
+            if channel.topic:
+                chars += len(channel.topic)
+
+            # If this page is full or there or no pages,
+            # create a new one
+            if chars > 900 or not sorted_channels:
+                sorted_channels.append([])
+                chars = 0
+
+            # Add the channel to the last page
+            sorted_channels[-1].append(channel)
+
+        # Create an embed for each page
+        all_embeds = (
+            HelpChannelsEmbed(channels) for channels in sorted_channels
+        )
+
+        # Create an object to manage the pages
+        multi_embed = EmbedPageManager()
+        multi_embed.add_embeds(*all_embeds)
+
+        # Send the pages to the member
+        await multi_embed.send(inter)
 
 
 async def setup(bot):
---
+++ src/ui/views.py
@@ -0,0 +1,76 @@
+"""Views for the bot"""
+
+import logging
+
+import discord
+from discord import Interaction as Inter
+
+
+log = logging.getLogger(__name__)
+
+
+class EmbedPageView(discord.ui.View):
+    """Controls for the embed page view"""
+
+    def __init__(self, multi_embed):
+        super().__init__(timeout=None)
+        self.multi_embed = multi_embed
+
+    async def _btn_event(self, inter:Inter, page:int):
+        """Handle the button press event"""
+
+        log.debug('Button pressed for page: %s', page)
+
+        if page not in range(0, self.multi_embed.pages):
+            log.debug('Invalid page: %s', page)
+            await self.multi_embed.send(inter)
+            return
+
+        log.debug('Moving to page: %s', page)
+
+        self.multi_embed.current_page = page
+        await self.multi_embed.send(inter)
+
+    async def update_buttons(self, inter:Inter):
+        """Update the buttons"""
+
+        log.debug('Updating buttons')
+
+        self.children[0].disabled = self.on_first_page
+        self.children[1].disabled = self.on_last_page
+
+        if inter.response.is_done():
+            await inter.edit_original_response(view=self)
+
+    @property
+    def on_first_page(self) -> bool:
+        """Check if the current page is the first page"""
+
+        return self.multi_embed.current_page == 0
+
+    @property
+    def on_last_page(self) -> bool:
+        """Check if the current page is the last page"""
+
+        return self.multi_embed.current_page \
+            == self.multi_embed.pages - 1
+
+    @discord.ui.button(
+        label='Prev Page',
+        style=discord.ButtonStyle.secondary,
+        custom_id='prev_page',
+    )
+    async def prev_page(self, inter:Inter, _):
+        """When the next page button has been pressed"""
+
+        await self._btn_event(inter, self.multi_embed.current_page - 1)
+
+    @discord.ui.button(
+        label='Next Page',
+        style=discord.ButtonStyle.secondary,
+        custom_id='next_page',
+    )
+    async def next_page(self, inter:Inter, _):
+        """When the next page button has been pressed"""
+
+        await self._btn_event(inter, self.multi_embed.current_page + 1)
