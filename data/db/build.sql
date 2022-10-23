7fafd2b9dd9939dde08b4c86cc10df4a607425ed
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-10-23 11:34:32 +0100

    Purposes added

--- data/db/build.sql
+++ data/db/build.sql
@@ -3,31 +3,50 @@ CREATE TABLE IF NOT EXISTS user_birthdays (
     birthday TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS tickets (
-    ticket_id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    active INTEGER NOT NULL DEFAULT 1,
-    extra_info TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS user_report_tickets (
-    ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    accused_user_id INTEGER NOT NULL,
-    channel_id INTEGER,
-    reason_msg TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS user_suggestion_tickets (
-    ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    channel_id INTEGER,
-    suggestion_msg TEXT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS member_levels (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     member_id INTEGER NOT NULL,
     guild_id INTEGER NOT NULL,
     experience INTEGER NOT NULL DEFAULT 1
 );
+
+CREATE TABLE IF NOT EXISTS guild_channels (
+    channel_id INTEGER PRIMARY KEY,
+    guild_id INTEGER NOT NULL,
+    purpose_id INTEGER NOT NULL,
+    FOREIGN KEY (purpose_id) REFERENCES channel_purposes(id)
+);
+
+-- Channel keys are used by modules in the bot to identify channels
+CREATE TABLE IF NOT EXISTS guild_channel_purposes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+
+-- Add the default channel keys
+INSERT OR IGNORE INTO guild_channel_purposes (name) VALUES ('general');
+INSERT OR IGNORE INTO guild_channel_purposes (name) VALUES ('welcome');
+INSERT OR IGNORE INTO guild_channel_purposes (name) VALUES ('goodbye');
+INSERT OR IGNORE INTO guild_channel_purposes (name) VALUES ('announcements');
+INSERT OR IGNORE INTO guild_channel_purposes (name) VALUES ('logs');
+INSERT OR IGNORE INTO guild_channel_purposes (name) VALUES ('bots');
+
+
+CREATE TABLE IF NOT EXISTS guild_roles (
+    role_id INTEGER PRIMARY KEY,
+    guild_id INTEGER NOT NULL,
+    purpose_id INTEGER NOT NULL,
+    FOREIGN KEY (purpose_id) REFERENCES role_purposes(id)
+);
+
+CREATE TABLE IF NOT EXISTS guild_role_purposes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+
+INSERT OR IGNORE INTO guild_role_purposes (name) VALUES ('member');
+INSERT OR IGNORE INTO guild_role_purposes (name) VALUES ('admin');
+INSERT OR IGNORE INTO guild_role_purposes (name) VALUES ('mod');
+INSERT OR IGNORE INTO guild_role_purposes (name) VALUES ('muted');
+INSERT OR IGNORE INTO guild_role_purposes (name) VALUES ('verified');
+INSERT OR IGNORE INTO guild_role_purposes (name) VALUES ('birthday');
