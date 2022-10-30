35a695a89613cb68a8991b40593009be77de33cd
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-10-30 23:34:02 +0000

    implemented ban command

--- src/ext/modtools.py
+++ src/ext/modtools.py
@@ -1,7 +1,8 @@
 """Server Moderation Tools"""
 
 import logging
-from datetime import datetime, timedelta, time
+import sqlite3
+from datetime import datetime, timedelta
 
 import discord
 from discord import (
@@ -18,9 +19,31 @@ from . import BaseCog
 
 log = logging.getLogger(__name__)
 
+def del_msg_choices():
+    choices = [
+        ("Don't Delete Any", 0),
+        ("Previous Day", 1)
+    ]
+    choices.extend((f"Previous {i} Days", i) for i in range(2, 8))
+    return [
+        app_commands.Choice(name=name, value=val)
+        for name, val in choices
+    ]
+
 
 class ModToolsCog(BaseCog, name="Moderation Tools"):
     """Cog for moderation tools"""
+
+    async def validate_user(self, inter:Inter, member_or_id) -> discord.User | None:
+        if type(member_or_id) is str:
+            try:
+                user = await self.bot.get.user(int(member_or_id))
+            except (discord.NotFound, ValueError):
+                user = None
+        else:
+            user = await self.bot.get.user(member_or_id.id)
+
+        return user
 
     @app_commands.command(name="kick")
     @app_commands.guild_only()
@@ -36,158 +59,75 @@ class ModToolsCog(BaseCog, name="Moderation Tools"):
         await member.kick(reason=reason)
         await inter.response.send_message(f"Kicked {member.name}")
 
+    # @app_commands.command(name="ban")
+    # @app_commands.guild_only()
+    # @app_commands.default_permissions(moderate_members=True)
+    # async def ban_cmd(
+    #     self,
+    #     inter:Inter,
+    #     member:discord.Member,
+    #     reason:str=None
+    # ):
+    #     """Ban a member from the server"""
+
+    #     await member.ban(reason=reason)
+    #     await inter.response.send_message(f"Banned {member.name}")
+
     @app_commands.command(name="ban")
     @app_commands.guild_only()
-    @app_commands.default_permissions(moderate_members=True)
-    async def ban_cmd(
-        self,
-        inter:Inter,
-        member:discord.Member,
-        reason:str=None
-    ):
-        """Ban a member from the server"""
-
-        await member.ban(reason=reason)
-        await inter.response.send_message(f"Banned {member.name}")
-
-    @app_commands.command(name="mute")
-    @app_commands.guild_only()
-    @app_commands.default_permissions(moderate_members=True)
-    @app_commands.describe(
-        member="The server member to mute",
-        minutes="The amount of minutes to mute the member for",
-        reason="The reason for the mute"
+    @app_commands.checks.bot_has_permissions(ban_members=True)
+    @app_commands.default_permissions(
+        moderate_members=True,
+        ban_members=True,
+        kick_members=True
     )
-    async def mute_cmd(
+    @app_commands.describe(
+        user="The user to ban, this can be a mention or their ID",
+        delete_messages="The amount of messages to delete from the user",
+        reason="The reason for the ban"
+    )
+    @app_commands.choices(delete_messages=del_msg_choices())
+    async def blacklist_cmd(
         self,
         inter:Inter,
-        member:discord.Member,
-        minutes:int=5,
+        user:discord.User,
+        delete_messages:app_commands.Choice[int],
         reason:str=None
     ):
-        """Mute a member in the server"""
+        """Bans a user from this server, even if they aren't in it"""
 
-        log.info(
-            "Muting %s for %s minutes in %s",
-            member, minutes, inter.guild.name
+        log.debug(
+            "%s is banning %s from %s for \"%s\" and deleting their "
+            "messages from the last %s days",
+            inter.user, user, inter.guild, reason, delete_messages.value
         )
 
-        mute_role_id = db.field(
-            "SELECT role_id FROM guild_roles "
-            "WHERE guild_id = ? AND purpose_id = ?",
-            inter.guild.id, RolePurposes.muted.value
-        )
+        username = f"{user.name}#{user.discriminator}"
 
-        mute_role = inter.guild.get_role(mute_role_id)
-
-        if not mute_role:
-            log.debug("Mute role not found, cancelling mute")
-            await inter.response.send_message(
-                "There is no mute role configured for this server, "
-                "use `/purpose roles` to configure one."
-            )
-            return
-
-        log.debug("Mute role found, muting member")
-
-        if mute_role in member.roles:
-            log.debug("Member is already muted, cancelling mute")
-            await inter.response.send_message(
-                "This member is already muted."
-            )
-            return
-
+        # Check if the user was already banned
         try:
-            await member.add_roles(mute_role, reason=reason)
-        except discord.Forbidden as err:
-            log.error(err)
-            await inter.response.send_message(content=err)
+            await inter.guild.fetch_ban(user)
+            log.debug("%s was already banned, cancelling", username)
+            await inter.response.send_message(
+                f"{username} is already banned.",
+                ephemeral=True
+            )
             return
 
-        log.debug("Mute role added, scheduling unmute")
+        except discord.NotFound:
+            log.debug("%s was not banned, continue", username)
 
-        now = datetime.now()
-        end = now + timedelta(minutes=minutes)
-        end = end.strftime(DATETIME_FORMAT)
 
-        db.execute(
-            "INSERT INTO guild_mutes "
-            "(member_id, guild_id, reason, end_datetime) "
-            "VALUES (?, ?, ?, ?)",
-            member.id, inter.guild.id, reason, end
+        await inter.guild.ban(
+            user=user, reason=reason,
+            delete_message_days=delete_messages.value
         )
-        db.commit()
+
+        log.debug("Banned %s", username)
 
         await inter.response.send_message(
-            f"Muted {member.name} for {minutes} minutes."
-        )
-
-    @tasks.loop(minutes=1)
-    async def check_mute_task(self):
-        """Check if mute timer has expired for each member"""
-
-        log.debug("Checking mute timers")
-
-        for guild in self.bot.guilds:
-
-            log.debug("Checking mute timers in %s", guild.name)
-
-            role_id = db.field(
-                "SELECT role_id FROM guild_roles "
-                "WHERE guild_id = ? AND purpose_id = ?",
-                guild.id, RolePurposes.muted.value
-            )
-
-            role = guild.get_role(role_id)
-
-            if not role:
-                log.debug("No mute role found, skipping")
-                continue
-
-            for member in guild.members:
-
-                if role not in member.roles:
-                    continue
-
-                try:
-                    await member.remove_roles(role)
-                except discord.Forbidden as err:
-                    log.error(err)
-                    continue
-
-                log.debug("Unmuted %s, removing from db", member.name)
-
-                db.execute(
-                    "DELETE FROM guild_mutes "
-                    "WHERE member_id = ? AND guild_id = ?",
-                    member.id, guild.id
-                )
-
-        db.commit()
-                
-
-
-    @app_commands.command(name="see-muted")
-    @app_commands.guild_only()
-    @app_commands.default_permissions(moderate_members=True)
-    async def see_muted_cmd(self, inter:Inter):
-        """Get a list of all muted members"""
-
-        muted_data = db.records(
-            "SELECT member_id, reason, end_datetime FROM guild_mutes "
-            "WHERE guild_id = ?",
-            inter.guild_id
-        )
-
-        if not muted_data:
-            await inter.response.send_message(
-                "There are no muted members in this server."
-            )
-            return
-
-        embed = ListMutedEmbed(self.bot, inter.guild, muted_data)
-        await inter.response.send_message(embed=embed)
-
+            f"I've just banned {username} from this server."
+        ) 
 
 async def setup(bot):
     """Cog setup function"""
