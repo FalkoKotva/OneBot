9748a65cb9f939ef4f951be5792fd679bf46ea82
Gregor Kšan <ksan.gregor@gmail.com>
2022-11-14 17:57:14 +0100

    Add Contribute Command #20

--- src/ext/contrib.py
+++ src/ext/contrib.py
@@ -2,7 +2,7 @@
 
 import logging
 from discord import app_commands, Interaction as Inter
-
+from discord import Embed, Color
 from . import BaseCog
 
 
@@ -19,7 +19,7 @@ class ContribCog(BaseCog, name='Contributions'):
         name='contrib',
         description='Contribution commands'
     )
-
+    
     @group.command(name='link')
     async def contribute(self, inter:Inter):
         """Link to contribute to the bot."""
@@ -28,7 +28,23 @@ class ContribCog(BaseCog, name='Contributions'):
             "https://github.com/XordK/OneBot",
             ephemeral=True
         )
-
+    
+    @group.command(name="learn")
+    async def contributeLearn(self, inter:Inter):
+        """Contributing to OneBot"""
+        
+        embed=Embed(title="Contributing to OneBot", url="https://github.com/XordK/OneBot", 
+                            description="If you want to help the further development of the bot, please get in touch or visit the github repository", 
+                            color= Color.blue())
+        
+        embed.set_author(name="XordK", url="https://github.com/XordK", icon_url="https://avatars.githubusercontent.com/u/77944149?v=4")
+        #Set a thumbnail via link.
+        #embed.set_thumbnail(url="")
+        
+        await inter.response.send_message(
+            embed=embed,
+            ephemeral=True
+        )
 
 async def setup(bot):
     """Setup function for the cog."""
