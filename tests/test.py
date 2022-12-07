60bb29e50cab5a13d46251335560e0b2e344ba1f
Gregor Kšan <ksan.gregor@gmail.com>
2022-12-07 17:13:14 +0100

    h

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ import discord.ext.test as dpytest
 
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.append('.')
+sys.path.append('OneBot/src')
 import bot as b
 
 @pytest.fixture
@@ -11,6 +11,7 @@ def bot(event_loop):
     
     with b.Bot() as bot: # However you create your bot, make sure to use loop=event_loop
         dpytest.configure(bot)
+        print("Hello")
         return bot
 
 
